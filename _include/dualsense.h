a3cc3c6ca2088c1811b1c9753ebc44501400d821
doukou-raz <nao.d180513@gmail.com>
2024-01-26 17:27:01 +0900

    コントローラーのボタン配置

---
+++ _include/dualsense.h
@@ -0,0 +1,22 @@
+#ifndef DUALSENSE_H
+#define DUALSENSE_H
+
+#define MARU_B 1
+#define SANKAKU_B 2
+#define BATU_B 0
+#define SIKAKU_B 3
+// #define UP_B 4
+// #define LEFT_B 7
+// #define DOWN_B 6
+// #define RIGHT_B 5
+#define LEFT_1 4
+#define LEFT_T 6
+#define RIGHT_1 5
+#define RIGHT_T 7
+#define LEFT_S_PUSH 11
+#define RIGHT_S_PUSH 12
+#define START 8
+#define SELECT 9
+#define PS 10
+
+#endif
---
+++ _include/dualshock3.h
@@ -0,0 +1,22 @@
+#ifndef DUALSHOCK3_H
+#define DUALSHOCK3_H
+
+#define MARU_B 13
+#define SANKAKU_B 12
+#define BATU_B 14
+#define SIKAKU_B 15
+#define UP_B 4
+#define LEFT_B 7
+#define DOWN_B 6
+#define RIGHT_B 5
+#define LEFT_1 10
+#define LEFT_T 8
+#define RIGHT_1 11
+#define RIGHT_T 9
+#define LEFT_S_PUSH 1
+#define RIGHT_S_PUSH 2
+#define START 3
+#define SELECT 0
+#define PS 16
+
+#endif
